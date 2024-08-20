d2338324e032bfd4a39c1c1c4ad73ff30b93757d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-08-20 13:15:15 +0600

    filter by price problem fix

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
             // Filter by price range
             const minPrice = parseFloat(req.query.minPrice) || 0;
             const maxPrice = parseFloat(req.query.maxPrice) || 1000;
-            query.price = { $gte: minPrice, $lte: maxPrice };
+            // query.price = { $gte: minPrice, $lte: maxPrice };
 
             // console.log(minPrice, maxPrice)
 
